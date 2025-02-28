234ac01f13be98ad03a6a2bf9f11c65d300b7651
enzomiguel7 <157727349+enzomiguel7@users.noreply.github.com>
2025-02-28 15:22:45 -0300

    Add project files.

---
+++ MenuItem.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RandomItemMenu
+{
+    internal class MenuItem
+    {
+        public Random randomizer = new Random();
+        public string[] Proteins = { "Roast beef", "Salami", "Turkey", "Ham", "Pastrami", "Tofu" };
+        public string[] Condiments = { "yellow mustard", "brown mustard", "honey mustard", "mayo", "relish", "french dressing" };
+        public string[] Breads = { "rye", "white", "wheat", "pumpernickel", "a roll" };
+
+        public string Description = "";
+        public string Price;
+
+    }
+}
